b52285f2ede8f640b0bbe99339a82d0f58dd4a98
Kuba Jaros <jarosk88@gmail.com>
2020-04-06 20:09:16 +0200

    remove logger from prod

--- src/components/collection-overview/collection-overview.component.jsx
+++ src/components/collection-overview/collection-overview.component.jsx
@@ -10,7 +10,7 @@ import './collection-overview.styles.scss';
 
 const CollectionOverview = ({ collections }) => (
 
-    <div className='shop-page'>{
+    <div className='collection-overview'>{
         collections.map(({ id, ...otherCollectionProps }) =>
             <CollectionPreview key={id} {...otherCollectionProps}></CollectionPreview>
         )
